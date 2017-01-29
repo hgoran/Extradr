40ef51584c3d4b5376b4f637b4995b4f8bda7d66
Damir <damir.cosic67@gmial.com>
2017-01-29 20:52:15 +0100

    aded bcrypt, jwt token, config

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,10 +1,21 @@
 const User = require('../models/user');
+const jwt = require('jwt-simple');
+const config = require('../config');
+
+function tokenForUser(user){
+	const timestamp = new Date().getTime();
+	return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
 
 exports.signup = function(req, res, next){
 	const username = req.body.username;
 	const email = req.body.email;
 	const password = req.body.password;
 
+	if (!username || !email || !password){
+		return res.status(422).send({ error: "Please provide username, email and password"});
+	}
+	// Do a check for username and password separately, and return a message error
 	// Check if username or email exists
 	User.findOne({ $or: [{ username: username}, {email: email }] }, function(err, existingUser){
 		if(err){ return next(err); }
@@ -23,7 +34,7 @@ exports.signup = function(req, res, next){
 		user.save(function(err){
 			if(err){return next(err);}
 			// Respond to request indicating user was created
-			res.json({ success: true });
+			res.json({ token: tokenForUser(user) });
 		});
 		
 	});
--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
 
 // Define our model
 const userSchema = new Schema({
@@ -7,6 +8,24 @@ const userSchema = new Schema({
 	email: { type: String, unique: true, lowercarse: true },
 	password: String
 });
+// On save hook, encrypt password
+// before saving the model run func
+userSchema.pre('save', function(next){
+	// get access to user model
+	const user = this;
+	// generate a salt then run callback
+	bcrypt.genSalt(10, function(err, salt){
+		if(err) { return next(err); }
+		// hash/encrypt password using salt 
+		bcrypt.hash(user.password, salt, null, function(err, hash){
+			if(err) { return next(err); }
+			// overwrite plain text password with encrypted password
+			user.password = hash;
+			next();
+		});	
+	});
+});
+
 // Create model class
 const ModelClass = mongoose.model('user', userSchema);
 // Export the model
