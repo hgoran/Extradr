f40e344937a4dfde969897027c594ce5614cf45a
Damir <damir.cosic67@gmial.com>
2017-01-31 22:10:33 +0100

    webpack configured

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -3,7 +3,11 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      	<div className="container">
+      		<h1 className="text-info jumbotron">
+      			React/Redux Starter
+      		</h1>
+      	</div>
     );
   }
 }
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,32 +1,106 @@
+// const webpack = require('webpack');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+const validate = require('webpack-validator');
+const parts = require('./libs/parts');
+const TARGET = process.env.npm_lifecycle_event;
 
 const PATHS = {
   app: path.join(__dirname, 'src'),
+  style: path.join(__dirname, 'style', 'style.css'),
   build: path.join(__dirname, 'build')
+};
+process.env.BABEL_ENV = TARGET;
+
+const common = {
+    // devtool: 'inline-source-map',
+    entry: {
+        style: PATHS.style,
+        app: PATHS.app
+    },
+    output: {
+        path: PATHS.build,
+        filename: '[name].js'
+    },
+    resolve: {
+        modulesDirectories: ['node_modules', 'src'],
+        extensions: ['', '.js','.jsx']
+    },
+    module: {
+        preLoaders: [
+          {
+            test: /\.jsx?$/,
+            loaders: ['eslint'],
+            include: PATHS.app
+          }
+        ],
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loaders: ['babel']
+            }
+        ]
+    },
+    plugins: [
+        // new webpack.HotModuleReplacementPlugin(),
+        // new webpack.NoErrorsPlugin(),
+        new HtmlWebpackPlugin({
+            template: 'index.html',
+            inject: 'body'
+        })
+    ]
+};
+
+var config;
+
+// Detect how npm is run and branch based on that
+switch(process.env.npm_lifecycle_event) {
+  case 'build':
+  case 'stats':
+    config = merge(
+      common,
+      {
+        devtool: 'source-map',
+        output: {
+          path: PATHS.build,
+          filename: '[name].[chunkhash].js',
+          // This is used for require.ensure. The setup
+          // will work without but this is useful to set.
+          chunkFilename: '[chunkhash].js'
+        }
+      },
+      parts.clean(PATHS.build),
+       parts.setFreeVariable(
+        'process.env.NODE_ENV',
+        'production'
+      ),
+       parts.extractBundle({
+        name: 'vendor',
+        entries: ['react', 'react-dom']
+      }),
+      parts.minify(),
+      parts.extractCSS(PATHS.style),
+      parts.purifyCSS([PATHS.app])
+    );
+    break;
+  default:
+    config = merge(
+      common,
+      {
+        devtool: 'eval-source-map'
+      },
+      parts.setupCSS(PATHS.style),
+      parts.devServer({
+        // Customize host/port here if needed
+        host: process.env.HOST,
+        port: process.env.PORT
+      })
+    );
 }
 
-module.exports = {
-  entry: {
-    app: PATHS.app,
-  },
-  output: {
-    path: PATHS.build,
-    filename: 'bundle.js'
-  },
-  module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015', 'stage-1']
-      }
-    }]
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
-  devServer: {
-    historyApiFallback: true,
-    contentBase: './'
-  }
-};
+// Run validator in quiet mode to avoid output in stats
+module.exports = validate(config, {
+  quiet: true
+});
