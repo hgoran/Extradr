b8a4f03553cd4090e10a56ba3d97a3759eb669a1
Damir Cosic <d.cosic@sportradar.com>
2017-02-14 15:10:21 +0100

    require_auth, feature, welcome component added. Added bootstrap cdn links

---
+++ client/src/components/welcome.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Welcome extends Component {
+	render(){
+		return(
+			<div>Welcome component - Index page</div>
+		);
+	}
+}
+
+export default Welcome;
--- client/src/index.js
+++ client/src/index.js
@@ -6,9 +6,12 @@ import { Router, Route, browserHistory, IndexRoute } from 'react-router';
 import reduxThunk from 'redux-thunk';
 
 import App from './components/app';
+import Feature from './components/feature';
+import Welcome from './components/welcome';
 import Signin from './components/auth/signin';
 import Signout from './components/auth/signout';
 import Signup from './components/auth/signup';
+import RequireAuth from './components/auth/require_auth';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
@@ -17,9 +20,11 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <Router history={browserHistory}>
     	<Route path="/" component={App}>
+      <IndexRoute component={Welcome}/>
 			<Route path="signin" component={Signin}/>
 			<Route path="signout" component={Signout}/>
 			<Route path="signup" component={Signup}/>
+      <Route path="feature" component={RequireAuth(Feature)}/>
     	</Route>
     </Router>
   </Provider>
